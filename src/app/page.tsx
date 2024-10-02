bd312cb8821152cfd92d4ba004c9d0663ab126dd
REDDY PRO <22135a0508@gvpce.ac.in>
2024-10-02 22:55:08 +0530

    google Auth added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
+    <div>
     <h1 className="text-3xl text-center">Hello sample git page</h1>
+
+    <Link href={"/auth/login"}>Go to login</Link>
+    </div>
   );
 }
