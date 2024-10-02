2962a196e8e72e45919f388337e4c1ab65dc2a6b
REDDY PRO <22135a0508@gvpce.ac.in>
2024-10-02 23:04:39 +0530

    google Auth added updated

---
+++ src/app/auth/login/LoginPage.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { auth } from "../../firebase/config";
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut, User } from "firebase/auth";
+import Image from 'next/image'; // Import Next.js Image component
+
+// Rename 'page' to 'LoginPage' or another suitable name starting with an uppercase letter
+export default function LoginPage() {
+  const [user, setUser] = useState<User | null>(null); // Initialize with null
+
+  const handleLogin = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setUser(null); // Reset user state on logout
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  useEffect(() => {
+    let isSubscribed = true; // Prevent side effects after unmount
+
+    onAuthStateChanged(auth, (currentUser) => {
+      if (isSubscribed) {
+        setUser(currentUser);
+      }
+    });
+
+    return () => {
+      isSubscribed = false;
+    };
+  }, []);
+
+  return (
+    <div className="h-screen grid content-center bg-stone-50 p-4">
+      {user ? (
+        <button
+          className="mx-auto bg-slate-800 max-w-[200px] p-2.5 rounded text-white text-lg text-center shadow-md"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      ) : (
+        <button
+          className="mx-auto bg-slate-800 max-w-[200px] p-2.5 rounded text-white text-lg text-center shadow-md"
+          onClick={handleLogin}
+        >
+          Continue with Google
+        </button>
+      )}
+      <div className="mt-10">
+        {user === null ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div>
+            <h2>User Details</h2>
+            <p>UserName: {user?.displayName}</p>
+            <p>Email: {user?.email}</p>
+            {user?.photoURL && (
+              <Image
+                src={user.photoURL}
+                width={40}
+                height={40}
+                className="rounded"
+                alt="User profile picture"
+              />
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
