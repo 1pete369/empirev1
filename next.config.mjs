3b5ca3c9ee6d1aeb391210fcaba3bafa4aa7a368
REDDY PRO <22135a0508@gvpce.ac.in>
2024-10-04 11:51:11 +0530

    GoogleAuth working

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https', // Define the protocol
+          hostname: 'lh3.googleusercontent.com', // Define the hostname
+          pathname: '/**', // Define the path (wildcard to allow any image from the domain)
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
