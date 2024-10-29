48c261aefacbc5bb4e5da2fe8ccede3e1f4fd58d
REDDY PRO <22135a0508@gvpce.ac.in>
2024-10-29 10:18:01 +0530

    Image url updated

--- src/app/contexts/UserProviderContext.tsx
+++ src/app/contexts/UserProviderContext.tsx
@@ -1,7 +1,7 @@
 // src/app/contexts/UserProviderContext.tsx
-"use client"; // This directive tells Next.js that this is a Client Component.
+"use client" // This directive tells Next.js that this is a Client Component.
 
-import React, { createContext, useState, useEffect, useContext } from 'react';
+import React, { createContext, useState, useEffect, useContext } from "react"
 import {
   GoogleAuthProvider,
   signInWithPopup,
@@ -10,195 +10,213 @@ import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   updateProfile,
-  User as FirebaseUser,
-} from "firebase/auth";
-import { auth } from '../firebase/config'; // Adjust the import path according to your project structure
-import { FirebaseError } from 'firebase/app';
-import { IsUserExist } from '../functions/IsUserExists'; // Ensure this is correctly imported
+  User as FirebaseUser
+} from "firebase/auth"
+import { auth } from "../firebase/config" // Adjust the import path according to your project structure
+import { FirebaseError } from "firebase/app"
+import { IsUserExist } from "../functions/IsUserExists" // Ensure this is correctly imported
 
 // Define the type for the unified user object
 export type FirebaseUserObject = {
-  uid: string;
-  email: string;
-  displayName?: string;
-  username?: string;
-  photoURL?: string;
-  provider: 'google' | 'email';
-  isEmailVerified: boolean;
-  createdAt: Date;
-  lastLoginAt: Date;
+  uid: string
+  email: string
+  displayName?: string
+  username?: string
+  photoURL?: string
+  provider: "google" | "email"
+  isEmailVerified: boolean
+  createdAt: Date
+  lastLoginAt: Date
   customData?: {
-    preferences?: any;
-    streak?: number;
-    goals?: string[];
-  };
-};
+    preferences?: any
+    streak?: number
+    goals?: string[]
+  }
+}
 
 // Context to provide user data globally
 type UserContextType = {
-  user: FirebaseUserObject | null;
-  error: string | null;
-  setError: React.Dispatch<React.SetStateAction<string | null>>;
-  handleGoogleLogin: () => Promise<void>;
-  handleEmailLogin: (email: string, password: string) => Promise<void>;
-  handleEmailSignup: (email: string, password: string, username: string) => Promise<void>;
-  handleLogout: () => Promise<void>;
-};
+  user: FirebaseUserObject | null
+  error: string | null
+  setError: React.Dispatch<React.SetStateAction<string | null>>
+  handleGoogleLogin: () => Promise<void>
+  handleEmailLogin: (email: string, password: string) => Promise<void>
+  handleEmailSignup: (
+    email: string,
+    password: string,
+    username: string
+  ) => Promise<void>
+  handleLogout: () => Promise<void>
+}
 
-const UserContext = createContext<UserContextType | null>(null);
+const UserContext = createContext<UserContextType | null>(null)
 
 // Function to map Firebase user to your user object
-const mapFirebaseUserToUserObject = (user: FirebaseUser, username?: string): FirebaseUserObject => {
+const mapFirebaseUserToUserObject = (
+  user: FirebaseUser,
+  username?: string
+): FirebaseUserObject => {
   return {
     uid: user.uid,
-    email: user.email || '',
-    displayName: user.displayName || '',
-    username: username || (user.displayName ? user.displayName.replace(/\s+/g, '') : ''),
-    photoURL: user.photoURL || 'https://source.unsplash.com/random/200x200',
-    provider: user.providerData[0]?.providerId === 'google.com' ? 'google' : 'email',
+    email: user.email || "",
+    displayName: user.displayName || "",
+    username:
+      username ||
+      (user.displayName ? user.displayName.replace(/\s+/g, "") : ""),
+    photoURL: user.photoURL || "https://picsum.photos/200",
+    provider:
+      user.providerData[0]?.providerId === "google.com" ? "google" : "email",
     isEmailVerified: user.emailVerified,
-    createdAt: new Date(user.metadata.creationTime || ''),
-    lastLoginAt: new Date(user.metadata.lastSignInTime || ''),
+    createdAt: new Date(user.metadata.creationTime || ""),
+    lastLoginAt: new Date(user.metadata.lastSignInTime || ""),
     customData: {
-      streak: 0,
-    },
-  };
-};
+      streak: 0
+    }
+  }
+}
 
-export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [user, setUser] = useState<FirebaseUserObject | null>(null);
-  const [error, setError] = useState<string | null>(null);
+export const UserProvider: React.FC<{ children: React.ReactNode }> = ({
+  children
+}) => {
+  const [user, setUser] = useState<FirebaseUserObject | null>(null)
+  const [error, setError] = useState<string | null>(null)
 
   const handleGoogleLogin = async () => {
     try {
-      const provider = new GoogleAuthProvider();
-      const result = await signInWithPopup(auth, provider);
-      const firebaseUser = result.user;
-      setUser(mapFirebaseUserToUserObject(firebaseUser));
-      setError(null);
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider)
+      const firebaseUser = result.user
+      setUser(mapFirebaseUserToUserObject(firebaseUser))
+      setError(null)
     } catch (err) {
-      console.error("Google login error:", err);
-      setError('An unexpected error occurred during Google login.');
+      console.error("Google login error:", err)
+      setError("An unexpected error occurred during Google login.")
     }
-  };
+  }
 
   const handleEmailLogin = async (email: string, password: string) => {
     if (password.length < 8) {
-      setError('Password must be at least 8 characters long.');
-      return;
+      setError("Password must be at least 8 characters long.")
+      return
     }
 
     try {
-      const result = await signInWithEmailAndPassword(auth, email, password);
-      const firebaseUser = result.user;
-      setUser(mapFirebaseUserToUserObject(firebaseUser));
-      setError(null);
+      const result = await signInWithEmailAndPassword(auth, email, password)
+      const firebaseUser = result.user
+      setUser(mapFirebaseUserToUserObject(firebaseUser))
+      setError(null)
     } catch (err) {
-      console.error("Email login error:", err);
+      console.error("Email login error:", err)
       if (err instanceof FirebaseError) {
-        setError('Invalid email/password');
-        console.log(err.message);
+        setError("Invalid email/password")
+        console.log(err.message)
       } else {
-        setError('An unexpected error occurred. Please try again.');
+        setError("An unexpected error occurred. Please try again.")
       }
     }
-  };
+  }
 
   const isValidEmail = (email: string) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  };
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return regex.test(email)
+  }
 
-  const handleEmailSignup = async (email: string, password: string, username: string) => {
+  const handleEmailSignup = async (
+    email: string,
+    password: string,
+    username: string
+  ) => {
     if (!isValidEmail(email)) {
-      setError('Invalid email format. Please check and try again.');
-      return;
+      setError("Invalid email format. Please check and try again.")
+      return
     }
 
     if (password.length < 8) {
-      setError('Password must be at least 8 characters long.');
-      return;
+      setError("Password must be at least 8 characters long.")
+      return
     }
 
     try {
-      const result = await createUserWithEmailAndPassword(auth, email, password);
-      const firebaseUser = result.user;
+      const result = await createUserWithEmailAndPassword(auth, email, password)
+      const firebaseUser = result.user
 
       await updateProfile(firebaseUser, {
-        displayName: username,
-      });
-      setUser(mapFirebaseUserToUserObject(firebaseUser, username));
-      setError(null);
+        displayName: username
+      })
+
+      setUser(mapFirebaseUserToUserObject(firebaseUser, username))
+      setError(null)
     } catch (err) {
-      console.error('Signup error:', err);
+      console.error("Signup error:", err)
       if (err instanceof FirebaseError) {
         switch (err.code) {
           case "auth/email-already-in-use":
-            setError("Email already exists!");
-            break;
+            setError("Email already exists!")
+            break
           default:
-            setError(err.message || "Signup failed. Please try again.");
-            break;
+            setError(err.message || "Signup failed. Please try again.")
+            break
         }
       } else {
-        setError("Signup failed. Please try again.");
+        setError("Signup failed. Please try again.")
       }
     }
-  };
+  }
 
   useEffect(() => {
     if (user !== null) {
-      console.log("User state updated:", user);
+      console.log("User state updated:", user)
       IsUserExist(user)
     }
-
-  }, [user]); // Dependency on `user`
+  }, [user]) // Dependency on `user`
 
   const handleLogout = async () => {
     try {
-      await signOut(auth);
-      setUser(null);
-      setError(null);
+      await signOut(auth)
+      setUser(null)
+      setError(null)
     } catch (err) {
       if (err instanceof FirebaseError) {
-        setError("Logout failed. Please try again.");
-        console.log(err.message);
+        setError("Logout failed. Please try again.")
+        console.log(err.message)
       }
     }
-  };
+  }
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
       if (firebaseUser) {
-        setUser(mapFirebaseUserToUserObject(firebaseUser));
-        setError(null);
+        setUser(mapFirebaseUserToUserObject(firebaseUser))
+        setError(null)
       } else {
-        setUser(null);
+        setUser(null)
       }
-    });
-    return () => unsubscribe();
-  }, []);
+    })
+    return () => unsubscribe()
+  }, [])
 
   return (
-    <UserContext.Provider value={{
-      user,
-      error,
-      handleGoogleLogin,
-      handleEmailLogin,
-      handleEmailSignup,
-      handleLogout,
-      setError,
-    }}>
+    <UserContext.Provider
+      value={{
+        user,
+        error,
+        handleGoogleLogin,
+        handleEmailLogin,
+        handleEmailSignup,
+        handleLogout,
+        setError
+      }}
+    >
       {children}
     </UserContext.Provider>
-  );
-};
+  )
+}
 
 // Custom hook to use the User context
 export const useUser = () => {
-  const context = useContext(UserContext);
+  const context = useContext(UserContext)
   if (context === null) {
-    throw new Error("useUser must be used within a UserProvider");
+    throw new Error("useUser must be used within a UserProvider")
   }
-  return context;
-};
+  return context
+}
